7cda806edafa15a58d1177c80670f4df2714b0ce
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2025-03-27 00:51:42 +0800

    Run batch

--- code/llm/pairwise.py
+++ code/llm/pairwise.py
@@ -1,5 +1,6 @@
 from openai import AsyncOpenAI
 import asyncio
+from tqdm import tqdm
 
 from fixed_endpoints.utils import load_pickle
 from .types import Path, Prompting
@@ -10,10 +11,9 @@ async def compare_paths(paths: list[Path], out: str, prompter: Prompting) -> Non
     with open(out, "a") as f:
         for a, b in pairs:
             answer = await prompter.query(a, b)
-            print("Answer:", answer, flush=True)
             f.write(f"{str(answer)}\n")
 
-async def main(filename: str, out: str, range: tuple[int, int]):
+async def run(filename: str, out: str, range: tuple[int, int]):
     # Range: for the purpose of cost monitoring
     data = load_pickle(filename)
     client = AsyncOpenAI()
@@ -24,9 +24,26 @@ async def main(filename: str, out: str, range: tuple[int, int]):
         tasks.append(compare_paths(paths, out, prompter))
     await asyncio.gather(*tasks)
 
+# Due to OpenAI rate limit, we can only do async 10 at a time
+async def run_batch(filename: str, out: str, clip: tuple[int, int], interval: int):
+    start, end = clip
+    for i in tqdm(list(range(start, end, interval))):
+        r = (i, i + interval)
+        await run(filename=filename, out=out, range=r)
+
+async def run_science(range: tuple[int, int], interval: int = 10):
+    await run_batch(filename="../data/fixed_endpoints/science_paths_fixed_endpoints.pkl",
+                    out="../data/fixed_endpoints/science_answers.txt",
+                    clip=range,
+                    interval=interval)
+
+async def run_money(range: tuple[int, int], interval: int = 10):
+    await run_batch(filename="../data/fixed_endpoints/money_paths_fixed_endpoints.pkl",
+                    out="../data/fixed_endpoints/money_answers.txt",
+                    clip=range,
+                    interval=interval)
+
 
 if __name__ == "__main__":
-    task = main(filename="../data/fixed_endpoints/science_paths_fixed_endpoints.pkl",
-                out="../data/fixed_endpoints/science_answers.txt",
-                range=(0, 10))
+    task = run_science(range=(10, 200))
     asyncio.run(task)
