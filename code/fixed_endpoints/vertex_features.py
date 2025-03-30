f31af2fdaf154ec36eac6930ed1dfded07dba877
calebcjl <calebchan2000@gmail.com>
2025-03-30 11:51:58 +0800

    get vertex degree from conceptnet web api

--- code/fixed_endpoints/vertex_features.py
+++ code/fixed_endpoints/vertex_features.py
@@ -3,6 +3,7 @@ import pickle
 import requests
 
 GLOVE_PATH = "../data/glove.42B.300d.txt"
+LIMIT = 1000
 
 def get_glove_features(vtxs: set[str]):
     vtx_emb = {}
@@ -19,18 +20,44 @@ def get_glove_features(vtxs: set[str]):
     return vtx_emb, vtx_freq
 
 # number of neighbours with no duplicates (even if there are multiple relations)
-def get_vertex_degree(vtx: str, vtxs: set[str]):
-    offset = 0
-    isEnd = False
-    while not isEnd:
-        obj = requests.get(f"http://api.conceptnet.io/c/en/{vtx}?offset={offset}").json() neighbours = set()
-        for edge in obj["edges"]:
+def get_vertex_degree(vtxs: set[str]):
+    vtx_deg = {}
+    
+    for vtx in vtxs:
+        offset = 0
+        neighbours = set()
+        isEnd = False
+        while not isEnd:
+            obj = requests.get(f"http://api.conceptnet.io/c/en/{vtx}?offset={offset}&limit={LIMIT}").json()
+            edges = obj["edges"]
+            if len(edges) < LIMIT:
+                isEnd = True
+            for edge in edges:
+                start = edge["start"]["label"]
+                end = edge["end"]["label"]
+                if vtx == start:
+                    neighbour = end
+                elif vtx == end:
+                    neighbour = start
+                else:
+                    print("ERROR: Vertex is not one of nodes")
+                
+                neighbours.add(neighbour)
+            offset += LIMIT
+        
+        vtx_deg[vtx] = len(neighbours)
+        
+    return vtx_deg
         
         
         
 def get_vertex_features(vtxs: set[str], features_folder: str):
+    vtx_deg = get_vertex_degree(vtxs)
     vtx_emb, vtx_freq = get_glove_features(vtxs)
     
+    with open(features_folder + "vertex_degree.pkl", "wb") as file:
+        pickle.dump(vtx_deg, file)
+        
     with open(features_folder + "vertex_embedding.pkl", "wb") as file:
         pickle.dump(vtx_emb, file)
         
